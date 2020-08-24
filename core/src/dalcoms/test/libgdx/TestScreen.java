b1d7e2a33e28480f79eb8ab6a15f87554089f25b
hslee <herangsa@gmail.coom>
2020-08-24 17:38:21 +0900

    test overlap

--- core/src/dalcoms/test/libgdx/TestScreen.java
+++ core/src/dalcoms/test/libgdx/TestScreen.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -13,6 +16,7 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import dalcoms.lib.libgdx.IGestureInput;
 import dalcoms.lib.libgdx.Renderable;
 import dalcoms.lib.libgdx.SpriteGameObject;
+import dalcoms.lib.libgdx.VariationPerTime;
 
 public class TestScreen implements Screen {
     private final MyGdxGame game;
@@ -69,7 +73,7 @@ public class TestScreen implements Screen {
     }
 
     private void draw(float delta) {
-        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
@@ -84,7 +88,83 @@ public class TestScreen implements Screen {
     }
 
     private void initGameObject() {
-        testRotate();
+//        testRotate();
+        testIntersection();
+    }
+
+    private void testIntersection() {
+        final SpriteGameObject sgoTriangle =
+                new SpriteGameObject(new Texture(Gdx.files.internal("triangle.png")),
+                                     249, 455).setSpriteBatch(this.game.getSpriteBatch());
+        SpriteGameObject sgoCircle =
+                new SpriteGameObject(new Texture(Gdx.files.internal("circle_200.png")),
+                                     421, 477).setSpriteBatch(this.game.getSpriteBatch());
+
+        final Circle c = new Circle(sgoCircle.getCenterLocationX(), sgoCircle.getLocationY(),
+                                    sgoCircle.getWidth() / 2);
+        final Polygon p =
+                new Polygon(new float[]{0, 0,
+                        sgoTriangle.getWidth() / 2f,
+                        sgoTriangle.getHeight(),
+                        sgoTriangle.getWidth(),
+                        0});
+        p.setOrigin(sgoTriangle.getWidth() / 2f,sgoTriangle.getHeight()/2f);
+        p.setPosition(sgoTriangle.getLocationX(), sgoTriangle.getLocationY());
+        final Polygon p2 = new Polygon(
+                circleToPolygon(sgoCircle.getWidth() / 2, sgoCircle.getHeight() / 2,
+                                sgoCircle.getWidth() / 2, 10));
+        p2.setPosition(sgoCircle.getLocationX(), sgoCircle.getLocationY());
+//        sgoTriangle.moveX(1000f, 5f);
+//        sgoTriangle.setEventListenerMoveX(new VariationPerTime.EventListener() {
+//            @Override public void onUpdate(float v, float v1) {
+//                p.setPosition(sgoTriangle.getLocationX(), sgoTriangle.getLocationY());
+//                if (Intersector.overlapConvexPolygons(p, p2)) {
+//                    sgoTriangle.setColor(1, 0, 0, 1);
+//                } else {
+//                    sgoTriangle.setColor(1, 1, 1, 1);
+//                }
+//            }
+//
+//            @Override public void onStart(float v) {
+//
+//            }
+//
+//            @Override public void onFinish(float v) {
+//
+//            }
+//        });
+sgoTriangle.rotate(8000,30);
+        sgoTriangle.setEventListenerRotate(new VariationPerTime.EventListener() {
+            @Override public void onUpdate(float v, float v1) {
+                p.setRotation(sgoTriangle.getRotationAngle());
+                if (Intersector.overlapConvexPolygons(p, p2)) {
+                    sgoTriangle.setColor(1, 0, 0, 1);
+                } else {
+                    sgoTriangle.setColor(1, 1, 1, 1);
+                }
+            }
+
+            @Override public void onStart(float v) {
+
+            }
+
+            @Override public void onFinish(float v) {
+
+            }
+        });
+
+        renderables.add(sgoTriangle, sgoCircle);
+    }
+
+    private float[] circleToPolygon(float centerX, float centerY, float radius, int verticesNum) {
+        float[] ret = new float[verticesNum * 2];
+        double angle = 2 * Math.PI / verticesNum;
+        for (int i = 0; i < verticesNum; i++) {
+            ret[i * 2] = (float) (centerX + radius * Math.cos(angle * i));
+            ret[i * 2 + 1] = (float) (centerY + radius * Math.sin(angle * i));
+            Gdx.app.log("deg", "result " + ret[i] + "," + ret[i + 1]);
+        }
+        return ret;
     }
 
     private void testRotate() {
@@ -94,9 +174,9 @@ public class TestScreen implements Screen {
         renderables.add(center);
         renderables.add(getCircle(getAngularX(center.getCenterLocationX(), radius, 0),
                                   getAngularY(center.getCenterLocationY(), radius, 0)));
-        for(int i=0;i<10;i++){
-            renderables.add(getCircle(getAngularX(center.getCenterLocationX(), radius, i*36),
-                                      getAngularY(center.getCenterLocationY(), radius, i*36)));
+        for (int i = 0; i < 10; i++) {
+            renderables.add(getCircle(getAngularX(center.getCenterLocationX(), radius, i * 36),
+                                      getAngularY(center.getCenterLocationY(), radius, i * 36)));
         }
     }
 
